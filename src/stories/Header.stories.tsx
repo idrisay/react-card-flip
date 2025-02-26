d7170248775101b17aae9fc0eae27028be0caf18
Idris AY <97283155+idrisay@users.noreply.github.com>
2025-02-26 22:23:20 +0100

    fix: remove storybook items

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -3,17 +3,4 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Header } from './Header';
 
-export default {
-  title: 'Example/Header',
-  component: Header,
-} as ComponentMeta<typeof Header>;
-
-const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
-
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  user: {},
-};
-
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+export default Header;
